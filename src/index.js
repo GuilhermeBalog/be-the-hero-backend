3d1d1bd7ecf7529f010df5166178412127813c80
Guilherme Balog Gardino <guilherme.gardino@usp.br>
2020-03-27 14:56:37 -0300

    Primeiras validações de rota

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const { errors } = require('celebrate')
 
 const routes = require('./routes')
 
@@ -8,6 +9,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(routes)
+app.use(errors())
 
 const port = process.env.PORT || 3333
 app.listen(port, () => {
