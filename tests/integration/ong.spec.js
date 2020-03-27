e08add9416f7ae3b6270413393d88ab712c8b834
Guilherme Balog Gardino <guilherme.gardino@usp.br>
2020-03-27 20:43:38 -0300

    adiciona a criação de caso no Teste de ONG

--- tests/integration/ong.spec.js
+++ tests/integration/ong.spec.js
@@ -3,6 +3,7 @@ const app = require('../../src/app')
 const connection = require('../../src/database/connection')
 
 describe('ONG', () => {
+
     beforeEach(async () => {
         await connection.migrate.rollback()
         await connection.migrate.latest()
@@ -12,16 +13,30 @@ describe('ONG', () => {
         await connection.destroy()
     })
 
-    it('should be able to create a new ONG', async () => {
-        const response = await request(app).post('/ongs').send({
-            name: "EcoMar",
-            email: "contato@ecomar.com",
-            whatsapp: "13900000000",
-            city: "Pedro de Toledo",
-            uf: "SP"
-        })
+    it('should be able to create a new ONG and an incident for this ONG', async () => {
+        const ongPostRresponse = await request(app)
+            .post('/ongs')
+            .send({
+                name: "EcoMar",
+                email: "contato@ecomar.com",
+                whatsapp: "13900000000",
+                city: "Pedro de Toledo",
+                uf: "SP"
+            })
 
-        expect(response.body).toHaveProperty('id')
-        expect(response.body.id).toHaveLength(8)
+        expect(ongPostRresponse.body).toHaveProperty('id')
+        expect(ongPostRresponse.body.id).toHaveLength(8)
+
+        const incidentPostResponse = await request(app)
+            .post('/incidents')
+            .set('authorization', ongPostRresponse.body.id)
+            .send({
+                title: "Cachorro ferido",
+                description: "Um foi cachorro ferido hoje",
+                value: 90
+            })
+
+        expect(incidentPostResponse.body).toHaveProperty('id')
+        expect(incidentPostResponse.body.id).toBeGreaterThan(0)
     })
 })
